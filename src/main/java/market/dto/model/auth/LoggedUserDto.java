faad927555c6d38e8deb7d1fd8d249b9dfa8f162
Henrique Bussolari <hbussular@hotmail.com>
2021-05-20 18:53:34 -0300

    Creates logic responsible for managing JWT token access and correspondent files

---
+++ src/main/java/market/dto/model/auth/LoggedUserDto.java
@@ -0,0 +1,26 @@
+package market.dto.model.auth;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import market.model.user.Role;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+@ToString
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class LoggedUserDto {
+    private String email;
+    private String fullName;
+    private List<Role> roles;
+}
